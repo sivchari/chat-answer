d527f2eee1730bc65bc3bdeb41340c9d2523db20
Yoshiki Fujikane <ffjlabo@gmail.com>
2023-06-18 17:10:01 +0900

    test: regenerate mock files

--- pkg/domain/repository/message/mock_message/mock_repository.go
+++ pkg/domain/repository/message/mock_message/mock_repository.go
@@ -49,3 +49,18 @@ func (mr *MockRepositoryMockRecorder) Insert(ctx, message interface{}) *gomock.C
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Insert", reflect.TypeOf((*MockRepository)(nil).Insert), ctx, message)
 }
+
+// SelectByRoomID mocks base method.
+func (m *MockRepository) SelectByRoomID(ctx context.Context, roomID string) ([]*entity.Message, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "SelectByRoomID", ctx, roomID)
+	ret0, _ := ret[0].([]*entity.Message)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// SelectByRoomID indicates an expected call of SelectByRoomID.
+func (mr *MockRepositoryMockRecorder) SelectByRoomID(ctx, roomID interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SelectByRoomID", reflect.TypeOf((*MockRepository)(nil).SelectByRoomID), ctx, roomID)
+}
--- pkg/usecase/chat/mock_chat/mock_interactor.go
+++ pkg/usecase/chat/mock_chat/mock_interactor.go
@@ -51,6 +51,36 @@ func (mr *MockInteractorMockRecorder) CreateRoom(ctx, name interface{}) *gomock.
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CreateRoom", reflect.TypeOf((*MockInteractor)(nil).CreateRoom), ctx, name)
 }
 
+// GetRoom mocks base method.
+func (m *MockInteractor) GetRoom(ctx context.Context, id string) (*entity.Room, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetRoom", ctx, id)
+	ret0, _ := ret[0].(*entity.Room)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetRoom indicates an expected call of GetRoom.
+func (mr *MockInteractorMockRecorder) GetRoom(ctx, id interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetRoom", reflect.TypeOf((*MockInteractor)(nil).GetRoom), ctx, id)
+}
+
+// ListMessage mocks base method.
+func (m *MockInteractor) ListMessage(ctx context.Context, roomID string) ([]*entity.Message, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "ListMessage", ctx, roomID)
+	ret0, _ := ret[0].([]*entity.Message)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// ListMessage indicates an expected call of ListMessage.
+func (mr *MockInteractorMockRecorder) ListMessage(ctx, roomID interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ListMessage", reflect.TypeOf((*MockInteractor)(nil).ListMessage), ctx, roomID)
+}
+
 // ListRoom mocks base method.
 func (m *MockInteractor) ListRoom(ctx context.Context) ([]*entity.Room, error) {
 	m.ctrl.T.Helper()
