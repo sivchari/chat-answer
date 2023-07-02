ee2bdc29bf8162c52b5bd5b0b8aae2bdb8f8e6cc
karamaru-alpha <mrnk3078@gmail.com>
2023-07-02 14:22:15 +0900

    dockerize bun

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+  },
   plugins: [react()],
   resolve: {
     alias: {
@@ -22,4 +25,3 @@ export default defineConfig({
     }
   }
 })
-
