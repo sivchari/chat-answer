cc736d5afb058169670c088551a390afa51e0901
karamaru-alpha <mrnk3078@gmail.com>
2023-07-07 22:54:29 +0900

    add: testcase

--- pkg/usecase/chat/interactor_test.go
+++ pkg/usecase/chat/interactor_test.go
@@ -2,6 +2,7 @@ package chat
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/golang/mock/gomock"
@@ -37,22 +38,34 @@ func newWithMocks(t *testing.T) (context.Context, Interactor, *mocks) {
 }
 
 func TestInteractor_CreateRoom(t *testing.T) {
-	id := "id"
-	name := "name"
+	t.Run("【正常系】", func(t *testing.T) {
+		id := "id"
+		name := "name"
 
-	ctx, i, m := newWithMocks(t)
-	m.ulidGenerator.EXPECT().Generate().Return(id, nil).Times(1)
-	m.roomRepository.EXPECT().Insert(ctx, &entity.Room{
-		ID:   id,
-		Name: name,
-	}).Return(nil).Times(1)
+		ctx, i, m := newWithMocks(t)
+		m.ulidGenerator.EXPECT().Generate().Return(id, nil).Times(1)
+		m.roomRepository.EXPECT().Insert(ctx, &entity.Room{
+			ID:   id,
+			Name: name,
+		}).Return(nil).Times(1)
 
-	res, err := i.CreateRoom(ctx, name)
-	assert.Equal(t, &entity.Room{
-		ID:   id,
-		Name: name,
-	}, res)
-	assert.NoError(t, err)
+		res, err := i.CreateRoom(ctx, name)
+		assert.Equal(t, &entity.Room{
+			ID:   id,
+			Name: name,
+		}, res)
+		assert.NoError(t, err)
+	})
+
+	t.Run("【異常系】nameが空文字列", func(t *testing.T) {
+		name := ""
+
+		ctx, i, _ := newWithMocks(t)
+
+		res, err := i.CreateRoom(ctx, name)
+		assert.Nil(t, res)
+		assert.Equal(t, errors.New("name is required"), err)
+	})
 }
 
 func TestInteractor_ListRoom(t *testing.T) {
@@ -71,4 +84,108 @@ func TestInteractor_ListRoom(t *testing.T) {
 		Name: name,
 	}}, res)
 	assert.NoError(t, err)
+}
+
+func TestInteractor_GetRoom(t *testing.T) {
+	t.Run("【正常系】", func(t *testing.T) {
+		roomID := "roomID"
+
+		ctx, i, m := newWithMocks(t)
+		m.roomRepository.EXPECT().Select(ctx, roomID).Return(&entity.Room{
+			ID: roomID,
+		}, nil).Times(1)
+
+		res, err := i.GetRoom(ctx, roomID)
+		assert.Equal(t, &entity.Room{
+			ID: roomID,
+		}, res)
+		assert.NoError(t, err)
+	})
+
+	t.Run("【異常系】roomIDが空", func(t *testing.T) {
+		roomID := ""
+
+		ctx, i, _ := newWithMocks(t)
+
+		res, err := i.GetRoom(ctx, roomID)
+		assert.Nil(t, res)
+		assert.Equal(t, errors.New("roomID is required"), err)
+	})
+}
+
+func TestInteractor_GetPass(t *testing.T) {
+	id := "id"
+
+	ctx, i, m := newWithMocks(t)
+	m.ulidGenerator.EXPECT().Generate().Return(id, nil).Times(1)
+
+	res, err := i.GetPass(ctx)
+	assert.Equal(t, id, res)
+	assert.NoError(t, err)
+}
+
+func TestInteractor_SendMessage(t *testing.T) {
+	t.Run("【正常系】", func(t *testing.T) {
+		roomID := "roomID"
+		text := "text"
+
+		ctx, i, m := newWithMocks(t)
+		m.messageRepository.EXPECT().Insert(ctx, &entity.Message{
+			RoomID: roomID,
+			Text:   text,
+		}).Return(nil).Times(1)
+
+		err := i.SendMessage(ctx, roomID, text)
+		assert.NoError(t, err)
+	})
+
+	t.Run("【異常系】roomIDが空文字列", func(t *testing.T) {
+		roomID := ""
+		text := "text"
+
+		ctx, i, _ := newWithMocks(t)
+
+		err := i.SendMessage(ctx, roomID, text)
+		assert.Equal(t, errors.New("roomID is required"), err)
+	})
+
+	t.Run("【異常系】textが空文字列", func(t *testing.T) {
+		roomID := "roomID"
+		text := ""
+
+		ctx, i, _ := newWithMocks(t)
+
+		err := i.SendMessage(ctx, roomID, text)
+		assert.Equal(t, errors.New("text is required"), err)
+	})
+}
+
+func TestInteractor_ListMessage(t *testing.T) {
+	t.Run("【正常系】", func(t *testing.T) {
+		roomID := "roomID"
+		text := "text"
+
+		ctx, i, m := newWithMocks(t)
+		m.messageRepository.EXPECT().SelectByRoomID(ctx, roomID).Return([]*entity.Message{{
+			RoomID: roomID,
+			Text:   text,
+		}}, nil).Times(1)
+
+		res, err := i.ListMessage(ctx, roomID)
+		assert.Equal(t, []*entity.Message{{
+			RoomID: roomID,
+			Text:   text,
+		}}, res)
+		assert.NoError(t, err)
+	})
+
+	t.Run("【異常系】roomIDが空文字列", func(t *testing.T) {
+		roomID := ""
+
+		ctx, i, _ := newWithMocks(t)
+
+		res, err := i.ListMessage(ctx, roomID)
+		assert.Nil(t, res)
+		assert.Equal(t, errors.New("roomID is required"), err)
+	})
 }
